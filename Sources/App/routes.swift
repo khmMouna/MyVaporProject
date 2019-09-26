b7eaa99069247b334a0900005a87e54d9d1998dc
Mouna khemiri <mkhemiri@accengage.com>
2019-09-26 16:02:12 +0200

    Improve leaf example

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -13,12 +13,35 @@ public func routes(_ router: Router) throws {
     }
 
     router.get("welcome") { req -> Future<View> in
-        return try req.view().render("welcome", ["name": "Leaf"])
+        //return try req.view().render("welcome", ["names": ["Jill", "Eve", "John"]])
+        let message1 = Message(id:"23456", alert:"banner", position:"Top")
+        let message2 = Message(id:"65378", alert:"interstitial", position:"Bottom")
+        let message3 = Message(id:"23876", alert:"banner", position:"Top")
+        let table = WelcomeContext(titles: ["Id", "Alert", "Message"], messages:[message1, message2, message3])
+        return try req.view().render("welcome", table)
     }
+    
     
     // Example of configuring a controller
     let todoController = TodoController()
     router.get("todos", use: todoController.index)
     router.post("todos", use: todoController.create)
     router.delete("todos", Todo.parameter, use: todoController.delete)
+    
+    struct Type: Codable {
+        var id:String
+        var name:String
+        
+    }
+    
+    struct Message: Codable {
+        var id: String
+        var alert: String
+        var position: String
+    }
+    
+    struct WelcomeContext: Codable {
+        var titles: [String]
+        var messages: [Message]
+    }
 }
